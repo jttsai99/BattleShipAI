532192d5d5246d320574354d4a1371867d311ab9
Jasper Tsai <jaspert99@gmail.com>
2020-02-19 23:21:33 -0800

    v3.2 working on search destroy, adding code deleted useless code

--- BattleShip/src/players/ais/search_destroy_ai.py
+++ BattleShip/src/players/ais/search_destroy_ai.py
@@ -43,13 +43,6 @@ class SearchDestroyAI(AIPlayer):
                     self.list_of_board_coords.remove(i)
                     return True
 
-    # def get_move(self):
-    #     if self.search_mode == True:
-    #         coords = self.select_random_from_list_all_coords()
-    #         firing_location = move.Move.from_str(self, coords)
-    #         self.delete_fireat_from_list()
-    #         return firing_location
-
     def get_move(self):
         if self.search_mode == True:
             coords = self.select_random_from_list_all_coords()
@@ -57,7 +50,7 @@ class SearchDestroyAI(AIPlayer):
         else:
             coords = (f'{self.circle_around_list[0][0]},{self.fireat[0][1]}')
             self.delete_circle()
-            firing_location = move.Move.from_str(self, coords)
+        firing_location = move.Move.from_str(self, coords)
         return firing_location
 
 
@@ -79,48 +72,6 @@ class SearchDestroyAI(AIPlayer):
             self.delete_from_circle_list()
 
 
-
-    # def check_return_status(self, status, hit_row, hit_col):
-    #     if (self.search_mode == True):
-    #         if (status == "Hit"):
-    #             self.search_mode = False
-    #             self.create_circle_around(hit_row, hit_col)
-    #     else:
-    #         self.saved_hit_cell.append(hit_row, hit_col)
-    #
-    # def get_move(self):
-    #     if (self.search_mode == True):
-    #         coords = self.select_random_from_list_all_coords()
-    #         # coords should be of form str-> row,col
-    #         firing_location = move.Move.from_str(self, coords)
-    #         self.delete_fireat_from_list()
-    #         return firing_location
-    #     else:
-    #         if (self.circle_around_list == []):
-    #           # now calculate remaining cells to be tryout
-    #           # will go either horizontal or vertical
-    #           # based on already fired cells from 'save_hit_cell'
-    #
-    #             if (self.save_hit_cell[0].row == self.save_hit_cell[1].row):
-    #                 dir = "Horizontal"
-    #             elif (self.save_hit_cell[0].col == self.save_hit_cell[1].col):
-    #                 dir = "Vertical"
-    #
-    #             # Put all the remaining cells to be Fired back in circle_list
-    #             if (dir == "Horizontal"):
-    #                 for cx in range(player.colmax):
-    #                     self.circle_around_list.append(saved_hit_cell[0].row, cx)
-    #             else:
-    #                 for cy in range(player.rowmax):
-    #                     self.circle_around_list.append(cy, saved_hit_cell[0].col)
-    #         else:
-    #             #  coords should be of str Type -> row,col
-    #             new_cordin = self.circle_around_list[0]
-    #             firing_location = move.Move.from_str(self, new_cordin)
-    #             return firing_location
-    #
-    #
-    #
     def create_circle_around(self): #eg: [[2,3],[5,2]....]
         self.circle_around_list = []
         self.save_hit_cell = []
@@ -129,17 +80,6 @@ class SearchDestroyAI(AIPlayer):
         self.circle_around_list.append([self.newrow, self.newcol+1])
         self.circle_around_list.append([self.newrow+1, self.newcol])
         return self.circle_around_list
-
-
-
-    # def get_move(self):
-    #
-    #     # coords should be string with "row,col"
-    #
-    #     coords = self.Search_Destroy_Fire_Method()
-    #
-    #     firing_location = move.Move.from_str(self, coords)
-    #     return firing_location
 
     def delete_circle(self):
         self.circle_around_list.pop(0)
